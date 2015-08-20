4bb0fc13b38a2572ed8c70c9524423fc812b5a8a
Wyns <pauwains@gmail.com>
2015-08-20 18:52:13 +0300

    added mote render

--- Documents/Rails_project/greeter/app.rb
+++ Documents/Rails_project/greeter/app.rb
@@ -1,15 +1,12 @@
 
 require "cuba"
+require "mote"
+require "mote/render"
+
+Cuba.plugin(Mote::Render)
 
 Cuba.define do
   on root do
-    res.write("
-    <html>
-        <body>
-          <h1>Kenya is my homeland country>
-          <p>central is my county</p>
-        </body>
-      </html>
-      ")
+    res.write("Hello Frogger!")
   end
 end
